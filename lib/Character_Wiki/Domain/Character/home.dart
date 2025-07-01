2ba4a30dfc1048215e2d36e9b9fb30105f92577a
KabeloBitcube <kabelo.intern@bitcube.tech>
2025-07-01 13:48:46 +0200

    UI update > Extracted drawer, bottom nav and body into methods. Made the body a single child scroll view to allow for scrolling when rotated

--- lib/Character_Wiki/Domain/Character/home.dart
+++ lib/Character_Wiki/Domain/Character/home.dart
@@ -20,155 +20,16 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(centerTitle: true, title: const Text('Rick & Morty Wiki')),
-      drawer: Drawer(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [const Text('Filters', style: TextStyle(fontSize: 15))],
-            ),
-            Padding(padding: const EdgeInsets.all(20), child: Divider()),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [const Text('Characters')],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: SizedBox(
-                height: 100,
-                child: ListView(
-                  scrollDirection: Axis.horizontal,
-                  children: [
-                    DropdownMenu(
-                      label: Text('Status'),
-                      dropdownMenuEntries: <DropdownMenuEntry<String>>[
-                        DropdownMenuEntry(value: 'Alive', label: 'Alive'),
-                        DropdownMenuEntry(value: 'Dead', label: 'Dead'),
-                        DropdownMenuEntry(value: 'Unknown', label: 'Unknown'),
-                      ],
-                    ),
-                    SizedBox(width: 10),
-                    DropdownMenu(
-                      label: Text('Species'),
-                      dropdownMenuEntries: <DropdownMenuEntry<String>>[
-                        DropdownMenuEntry(value: 'Human', label: 'Human'),
-                        DropdownMenuEntry(value: 'Alien', label: 'Alien'),
-                        DropdownMenuEntry(value: 'Robot', label: 'Robot'),
-                      ],
-                    ),
-                    SizedBox(width: 10),
-                    DropdownMenu(
-                      label: Text('Gender'),
-                      dropdownMenuEntries: <DropdownMenuEntry<String>>[
-                        DropdownMenuEntry(value: 'Female', label: 'Female'),
-                        DropdownMenuEntry(value: 'Male', label: 'Male'),
-                        DropdownMenuEntry(value: 'Unknown', label: 'Unknown'),
-                      ],
-                    ),
-                    SizedBox(width: 10),
-                  ],
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [const Text('Episode')],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: SizedBox(
-                height: 100,
-                child: ListView(
-                  scrollDirection: Axis.horizontal,
-                  children: [
-                    DropdownMenu(
-                      label: Text('Choose'),
-                      dropdownMenuEntries: <DropdownMenuEntry<String>>[
-                        DropdownMenuEntry(
-                          value: 'Episode 1',
-                          label: 'Episode 1',
-                        ),
-                        DropdownMenuEntry(
-                          value: 'Episode 2',
-                          label: 'Episode 2',
-                        ),
-                        DropdownMenuEntry(
-                          value: 'Episode 3',
-                          label: 'Episode 3',
-                        ),
-                      ],
-                    ),
+      drawer: drawerBuilder(),
+      bottomNavigationBar: bottomNavBuilder(),
+      body: bodyBuilder(context),
+    );
+  }
 
-                    SizedBox(width: 10),
-                  ],
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [const Text('Location')],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: SizedBox(
-                height: 100,
-                child: ListView(
-                  scrollDirection: Axis.horizontal,
-                  children: [
-                    DropdownMenu(
-                      label: Text('Choose'),
-                      dropdownMenuEntries: <DropdownMenuEntry<String>>[
-                        DropdownMenuEntry(
-                          value: 'Location 1',
-                          label: 'Location 1',
-                        ),
-                        DropdownMenuEntry(
-                          value: 'Location 2',
-                          label: 'Location 2',
-                        ),
-                        DropdownMenuEntry(
-                          value: 'Location 3',
-                          label: 'Location 3',
-                        ),
-                      ],
-                    ),
-
-                    SizedBox(width: 10),
-                  ],
-                ),
-              ),
-            ),
-            SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text('Clear Filters'),
-            ),
-          ],
-        ),
-      ),
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.only(bottom: 50),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            GestureDetector(onTap: () {}, child: Text('Characters')),
-            GestureDetector(onTap: () {}, child: Text('Episode')),
-            GestureDetector(onTap: () {}, child: Text('Location')),
-          ],
-        ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
+  Padding bodyBuilder(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(5.0),
+      child: SingleChildScrollView(
         child: Column(
           children: [
             Row(
@@ -244,12 +105,182 @@ class _HomeState extends State<Home> {
                 } else if (state is CharacterError) {
                   return Center(child: Text('Error: ${state.message}'));
                 }
-
+        
                 return const SizedBox.shrink();
               },
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  Padding bottomNavBuilder() {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 50),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          MaterialButton(
+            onPressed: () {
+              context.read<CharacterBloc>().add(const FetchCharacters());
+            },
+            child: Text('Characters'),
+          ),
+          MaterialButton(onPressed: () {}, child: Text('Episode')),
+          MaterialButton(onPressed: () {}, child: Text('Location')),
+        ],
+      ),
+    );
+  }
+
+  Drawer drawerBuilder() {
+    return Drawer(
+      child: ListView(
+        scrollDirection: Axis.vertical,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              DrawerHeader(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'Filters',
+                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [const Text('Characters')],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SizedBox(
+                  height: 100,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                      DropdownMenu(
+                        label: Text('Status'),
+                        dropdownMenuEntries: <DropdownMenuEntry<String>>[
+                          DropdownMenuEntry(value: 'Alive', label: 'Alive'),
+                          DropdownMenuEntry(value: 'Dead', label: 'Dead'),
+                          DropdownMenuEntry(value: 'Unknown', label: 'Unknown'),
+                        ],
+                      ),
+                      SizedBox(width: 10),
+                      DropdownMenu(
+                        label: Text('Species'),
+                        dropdownMenuEntries: <DropdownMenuEntry<String>>[
+                          DropdownMenuEntry(value: 'Human', label: 'Human'),
+                          DropdownMenuEntry(value: 'Alien', label: 'Alien'),
+                          DropdownMenuEntry(value: 'Robot', label: 'Robot'),
+                        ],
+                      ),
+                      SizedBox(width: 10),
+                      DropdownMenu(
+                        label: Text('Gender'),
+                        dropdownMenuEntries: <DropdownMenuEntry<String>>[
+                          DropdownMenuEntry(value: 'Female', label: 'Female'),
+                          DropdownMenuEntry(value: 'Male', label: 'Male'),
+                          DropdownMenuEntry(value: 'Unknown', label: 'Unknown'),
+                        ],
+                      ),
+                      SizedBox(width: 10),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [const Text('Episode')],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SizedBox(
+                  height: 100,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                      DropdownMenu(
+                        label: Text('Choose'),
+                        dropdownMenuEntries: <DropdownMenuEntry<String>>[
+                          DropdownMenuEntry(value: 'Episode 1', label: 'Episode 1'),
+                          DropdownMenuEntry(value: 'Episode 2', label: 'Episode 2'),
+                          DropdownMenuEntry(value: 'Episode 3', label: 'Episode 3'),
+                        ],
+                      ),
+          
+                      SizedBox(width: 10),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [const Text('Location')],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SizedBox(
+                  height: 100,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                      DropdownMenu(
+                        label: Text('Choose'),
+                        dropdownMenuEntries: <DropdownMenuEntry<String>>[
+                          DropdownMenuEntry(
+                            value: 'Location 1',
+                            label: 'Location 1',
+                          ),
+                          DropdownMenuEntry(
+                            value: 'Location 2',
+                            label: 'Location 2',
+                          ),
+                          DropdownMenuEntry(
+                            value: 'Location 3',
+                            label: 'Location 3',
+                          ),
+                        ],
+                      ),
+          
+                      SizedBox(width: 10),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Icon(Icons.clear, color: Colors.white,),
+                      SizedBox(width: 5),
+                      const Text('Clear Filters', style: TextStyle(color: Colors.white),),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
