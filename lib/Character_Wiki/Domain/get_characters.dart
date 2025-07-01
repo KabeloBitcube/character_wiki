8b85115057dfa5a987a2f84381c15bce3e219400
KabeloBitcube <kabelo.intern@bitcube.tech>
2025-07-01 09:26:56 +0200

    Character API > Domain layer

---
+++ lib/Character_Wiki/Domain/get_characters.dart
@@ -0,0 +1,20 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:character_wiki/Character_Wiki/Domain/character.dart';
+import 'package:character_wiki/Character_Wiki/Domain/character_repository.dart';
+
+class GetCharacters {
+  final CharacterRepository repository;
+
+  GetCharacters({
+    required this.repository,
+  });
+  
+  Future<List<Character>> call ({String? name, String? status, String? species, String? gender}) async {
+    return repository.getCharacters(
+      name: name,
+      status: status,
+      species: species,
+      gender: gender
+    );
+  }
+}
